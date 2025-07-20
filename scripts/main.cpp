5999ac20eb7654fd52f2ec1de60e30009eede1cc
Brennan L <bdlawson115@gmail.com>
2025-07-19 23:31:29 -0400

    get a rectangle on the screen!
    shader class, shader files, etc etc

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -12,7 +12,7 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(1080, 1080, "TeenyTinyBits", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1600, 1280, "TeenyTinyBits", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -28,12 +28,126 @@ int main(void)
 	}
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetKeyCallback(window, key_callback);
+
+	float vertices[] = {
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	}; 
+
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	std::ifstream vertSourceFile("./shaders/shader.vert", std::ios::binary);
+
+	if (!vertSourceFile)
+	{
+		std::cerr << "vertSourceFile could not be opened.";
+		return -1;
+	}
+
+	std::stringstream vertSourceStream;
+	vertSourceStream << vertSourceFile.rdbuf();
+	std::string vertSource = vertSourceStream.str();
+	const char* vertSourceConst = vertSource.c_str();
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertSourceConst, NULL);
+	glCompileShader(vertexShader);
+
+	GLint success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		return -1;
+	}
+
+
+	std::ifstream fragSourceFile("./shaders/shader.frag", std::ios::binary);
+
+	if (!fragSourceFile)
+	{
+		std::cerr << "fragSourceFile could not be opened.\n";
+		return -1;
+	}
+
+	std::stringstream fragSourceStream;
+	fragSourceStream << fragSourceFile.rdbuf();
+	std::string fragSource = fragSourceStream.str();
+	const char* fragSourceConst = fragSource.c_str();
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragSourceConst, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+		return -1;
+	}
+
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cerr << infoLog << std::endl;
+		return -1;
+	}
+
+	glUseProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+
+	// ..:: Initialization code :: ..
+	// 1. bind Vertex Array Object
+	glBindVertexArray(VAO);
+	// 2. copy our vertices array in a vertex buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 3. copy our index array in a element buffer for OpenGL to use
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	// 4. then set the vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0); 
 
 	while (!glfwWindowShouldClose(window))
 	{
-    	processInput(window);
+		/// rendering
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-		render(window);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+		/// end rendering
+
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();    
@@ -45,15 +159,28 @@ int main(void)
 
 void render(GLFWwindow* window)
 {
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
 }
 
-void processInput(GLFWwindow *window)
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 	{
         glfwSetWindowShouldClose(window, true);
+	}
+
+	if (key == GLFW_KEY_W && action == GLFW_PRESS)
+	{
+		GLint polygonMode;
+		glGetIntegerv(GL_POLYGON_MODE, &polygonMode);
+
+		if (polygonMode == GL_FILL)
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		}
+		else
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		}
 	}
 }
 
--- scripts/main.hpp
+++ scripts/main.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 // std library
+#include <fstream> // IWYU pragma: keep
+#include <sstream> // IWYU pragma: keep
 #include <iostream> // IWYU pragma: keep
 #include <cstdio>
 
@@ -15,7 +17,7 @@ int main(void);
 
 void render(GLFWwindow* window);
 
-static void windowInit(GLFWwindow* window);
+static void window_init(GLFWwindow* window);
 
-void processInput(GLFWwindow* window);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);  
