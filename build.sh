707af27f8be75f98ed47e6073ddc10bbda84aea1
Brennan L <bdlawson115@gmail.com>
2025-12-31 19:04:56 -0500

    folder restructuring!

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p "./build/"
-cd "./build/"
-
-cmake ".." -DCMAKE_EXPORT_COMPILE_COMMANDS=1
-
-cmake --build "." $@
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if ./build.sh ; then
-  printf "\n"
-  cd ./build/
-  ./TeenyTinyBits
-fi
